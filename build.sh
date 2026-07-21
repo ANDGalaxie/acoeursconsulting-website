85ff36adcd0a951aa1038f260426c735aa571b77
ANDGalaxie <sidake025@gmail.com>
2026-07-21 12:19:42 +0800

    fix: install dependencies during Render build

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "Installing Python dependencies..."
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+echo "Applying database migrations..."
 python manage.py migrate --noinput
